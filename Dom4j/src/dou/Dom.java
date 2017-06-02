3429764c9cd6c1e247c07e88544dfadca6923b5f
douxu896 <850207176@qq.com>
2017-06-02 16:16:39 +0800

    parsexml

--- Dom4j/src/dou/Dom.java
+++ Dom4j/src/dou/Dom.java
@@ -1,26 +1,25 @@
 package dou;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.IllegalFormatException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
-
 import com.mysql.jdbc.PreparedStatement;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 
 public class Dom {
 
@@ -28,111 +27,65 @@ public class Dom {
 		// TODO Auto-generated method stub
 		SAXReader reader = new SAXReader();
 		reader.setEntityResolver(new IgnoreDTDEntityResolver());
-		String path = "/Users/douxu/Desktop" + "/Adm_Policy_Ment_Health_2010_Mar_15_37(1-2)_201-204.nxml";
-		// Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+		String path = "/Users/douxu/Desktop/Adm_Policy_Ment_Health" + "/Adm_Policy_Ment_Health_2010_Mar_24_37(1-2)_15-26.nxml";
 		Document doc = reader.read(new File(path));
-		// 取得文章名，引言和正文
+		// get title,abstract,body
 		Element title = (Element) doc.selectObject("/article/front/article-meta/title-group/article-title");
 		Element abstractt = (Element) doc.selectObject("/article/front/article-meta/abstract");
 		Element body = (Element) doc.selectObject("article/body");
 		List<String> data = new ArrayList<String>();
-		System.out.println(title.getName());
-		System.out.println(title.getText());
-		// Text(title, data);
-		System.out.println(abstractt.getName());
+		System.out.println(title.getName() + ":" + title.getText());
+		System.out.println(abstractt.getName() + ":");
 		Text(abstractt, data);
-		System.out.println(body.getName());
+		System.out.println(body.getName() + ":");
 		Text(body, data);
 		JDBC db = new JDBC();
 		Sentence sen;
-		 String sql="insert into sentence(citation,sentences) values (?,?)";
+		String sql = "insert into sentence(citation,sentences) values (?,?)";
 	}
 
-	private static void exportCsv(File file, List<String> dataList) {
-		// TODO 自动生成的方法存根
-		FileOutputStream out = null;
-		OutputStreamWriter osw = null;
-		BufferedWriter bw = null;
-		try {
-			out = new FileOutputStream(file);
-			osw = new OutputStreamWriter(out);
-			bw = new BufferedWriter(osw);
-			if (dataList != null && !dataList.isEmpty()) {
-				for (String data : dataList) {
-					bw.append(data).append("\r");
-				}
-			}
-			// isSucess=true;
-		} catch (Exception e) {
-			// isSucess=false;
-		} finally {
-			if (bw != null) {
-				try {
-					bw.close();
-					bw = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (osw != null) {
-				try {
-					osw.close();
-					osw = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (out != null) {
-				try {
-					out.close();
-					out = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
-	private static void Text(Element node, List<String> data) {
-		// TODO 自动生成的方法存根
-		/*
-		 * if(!node.getText().equals("")) { System.out.println(node.getText());
-		 * }
-		 */
-		Iterator it = node.elementIterator();
+	private static void Text(Element element, List<String> data) {
+		Iterator it = element.elementIterator();
 		while (it.hasNext()) {
-			// System.out.println("======");
-			Element element = (Element) it.next();
-			Text(element, data);
-			  System.out.println(element.attributeValue("ref-type"));
-			Iterator iter = element.attributeIterator();
-			while (iter.hasNext()) {
-            
-              if( iter.next().toString().matches(""));
+			Element ele = (Element) it.next();
+			//System.out.println("name:" + ele.getName());
+			if (ele.getName().equals("p")) {
+				int num = ele.content().size();
+				List content = ele.content();
+				//System.out.println("元素个数：" + num);
+				String Name = ele.getName();
+				for (int cindex = 0; cindex < num; cindex++) {
+					Object first = content.get(cindex);
+					String firstText = getContentAsText(first);
+					System.out.print(firstText);
+				}
+				System.out.println(" ");
+			} else {
+				/*if (!(ele.getText().equals(""))) {
+					System.out.println(ele.getText());
+				}*/
+				Text(ele, data);
 			}
-			if (!element.getText().equals("")) {
-				data.add(element.getText());
-				System.out.println(element.getText());
-			}
-
 		}
 	}
 
-	private static void SavaXml(String fileName) {
-		Document document = DocumentHelper.createDocument();// 建立document对象，用来操作xml文件
-		Element booksElement = document.addElement("books");// 建立根节点
-		booksElement.addComment("This is a test for dom4j ");// 加入一行注释
-		Element bookElement = booksElement.addElement("book");// 添加一个book节点
-		bookElement.addAttribute("show", "yes");// 添加属性内容
-		Element titleElement = bookElement.addElement("title");// 添加文本节点
-		titleElement.setText("ajax in action");// 添加文本内容
-		try {
-			XMLWriter writer = new XMLWriter(new FileWriter(new File(fileName)));
-			writer.write(document);
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static String getContentAsText(Object content) {
+		if (content instanceof Node) {
+			Node node = (Node) content;
+			int type = node.getNodeType();
+			switch (type) {
+			case 1:
+				return node.getText();
+			case 3:
+				return node.getText();
+
+			default:
+				break;
+			}
+		} else if (content instanceof String) {
+			return (String) content;
 		}
+
+		return "";
 	}
 }
